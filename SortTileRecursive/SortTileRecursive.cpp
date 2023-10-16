a170eaf4004353b984aea4585dc4615b33ef903c
feliu <jorgefeliu.ag@gmail.com>
2023-10-16 03:42:26 -0300

    Update SortTileRecursive.cpp

--- SortTileRecursive/SortTileRecursive.cpp
+++ SortTileRecursive/SortTileRecursive.cpp
@@ -2,11 +2,6 @@
 #include <vector>
 using namespace std;
 
-//Estructura Punto
-struct Punto{
-    double x , y;
-};
-
 //Estructura Rectángulo
 struct Rectangulo{
     double x1, y1, x2, y2;
@@ -15,31 +10,31 @@ struct Rectangulo{
 //Estructura Nodo que posee rectangulo con sus hijos
 struct Nodo{
     Rectangulo MBR;
-    vector<Rectangulo> Rectangulos;
-    vector<Nodo*> Hijos;
+    vector<Rectangulo> rectangulos;
+    vector<Nodo*> hijos;
 };
 
 //Esta funcion construye el R-Tree de manera recursiva
-void ConstruirRtree(int M, vector<Punto> puntos){
+void ConstruirRtree(int M, vector<Rectangulo> rectangulos){
     
     //Ordenando los primeros n puntos segun su coordenada X
-    sort(puntos.begin(), puntos.end(), [](const Punto& izq, const Punto& der) {
-        return izq.x < der.x;
+    sort(rectangulos.begin(), rectangulos.end(), [](const Rectangulo& izq, const Rectangulo& der) {
+        return min(izq.x1,izq.x2) < min(der.x1, der.x2);
     });
 
     //Declarando la cantidad de grupos que se van a formar
-    int n = puntos.size();
+    int n = rectangulos.size();
     int S = sqrt(n/M);
 
     //Declarando los grupos de puntos segun su coordenada X
-    vector<vector<Punto>> GruposX;
+    vector<vector<Rectangulo>> GruposX;
     for (int i = 0; i < n; i += (M * S)) {
-        vector<Punto> GrupoX(puntos.begin() + i, puntos.begin() + min(i + (M * S), (int)n));
+        vector<Rectangulo> GrupoX(rectangulos.begin() + i, rectangulos.begin() + min(i + (M * S), (int)n));
         GruposX.push_back(GrupoX);
     }
 
     //Creando rectangurlos para cada grupo de puntos segun su posicion en X
-    vector<Rectangulo> rectangulos;
+    vector<Nodo> hyperrectangulos;
     for (auto GrupoX : GruposX) {
         double xmax = -1e9, xmin = 1e9, ymax = -1e9, ymin = 1e9;
         for (auto punto : GrupoX) {
@@ -48,12 +43,14 @@ void ConstruirRtree(int M, vector<Punto> puntos){
             ymax = max(ymax, punto.y);
             ymin = min(ymin, punto.y);
         }
-        Rectangulo rectangulo = {xmin, ymin, xmax, ymax};
-        rectangulos.push_back(rectangulo);
+        HyperRectangulo hyperrectangulo = {xmin, ymin, xmax, ymax};
+        hyperrectangulos.push_back(hyperrectangulo);
     }
 
+
+
     //Creamos recursivamente los nodos del R-Tree
-    Nodo* raiz = construirRecursiva(M, rectangulos);
+
 
 };
 
