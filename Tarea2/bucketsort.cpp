36c9bc714583ae67c30b9067c8836914e806613c
Andrés Arriagada <103340290+Pasta212@users.noreply.github.com>
2023-11-04 16:57:48 -0300

    Radixsort(poner a prueba)

--- Tarea2/bucketsort.cpp
+++ Tarea2/bucketsort.cpp
@@ -1,32 +1,26 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-using namespace std;
 
-void bucketSort(vector<unsigned long long int>& arr) {
+void bucketSort(std::vector<unsigned long long int>& arr, int k) {
     if (arr.empty()) return;
 
-    unsigned long long int maxValue = *max_element(arr.begin(), arr.end());
+    unsigned long long int maxValue = *std::max_element(arr.begin(), arr.end());
 
-    // Crear "buckets" (contenedores)
     int n = arr.size();
-    const int numBuckets = 10;  // número de buckets
-    vector<vector<unsigned long long int>> buckets(numBuckets);
+    int numBuckets = 1 << k;  // 2^k buckets
+    std::vector<std::vector<unsigned long long int>> buckets(numBuckets);
 
-    // Distribuir los elementos en los buckets
     for (int i = 0; i < n; i++) {
-        int bucketIndex = (arr[i] * numBuckets) / (maxValue + 1);
-        buckets[bucketIndex].push_back(arr[i]);
+        unsigned long long int key = (arr[i] >> k) & (numBuckets - 1);
+        buckets[key].push_back(arr[i]);
     }
 
-    // Ordenar los elementos dentro de cada bucket (si los hubiera)
     for (int i = 0; i < numBuckets; i++) {
         if (!buckets[i].empty()) {
-            sort(buckets[i].begin(), buckets[i].end());
+            std::sort(buckets[i].begin(), buckets[i].end());
         }
     }
-
-    // Combinar los elementos de los buckets en el arreglo original
     int index = 0;
     for (int i = 0; i < numBuckets; i++) {
         for (int j = 0; j < buckets[i].size(); j++) {
@@ -35,25 +29,28 @@ void bucketSort(vector<unsigned long long int>& arr) {
     }
 }
 
-
+void radixSort(std::vector<unsigned long long int>& arr, int maxBits) {
+    for (int k = 1; k <= maxBits; k++) {
+        bucketSort(arr, k);
+        std::cout << "Array after sorting with " << k << " bits: ";
+        for (unsigned long long int num : arr) {
+            std::cout << num << " ";
+        }
+        std::cout << std::endl;
+    }
+}
 
 int main() {
-    vector<unsigned long long int> arr = {9876543210ULL, 1234567890ULL, 5555555555ULL, 9999999999ULL, 1111111111ULL, 7777777777ULL};
-    
-    cout << "Arreglo original: ";
-    for (int i = 0; i < arr.size(); i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
+    std::vector<unsigned long long int> arr = {170, 45, 75, 90, 802, 24, 2, 66, 1, 8};
+    int maxBits = 8; // Definir el número máximo de bits a considerar
 
-    // Aplicar Bucket Sort al arreglo
-    bucketSort(arr);
+    radixSort(arr, maxBits);
 
-    cout << "Arreglo ordenado: ";
-    for (int i = 0; i < arr.size(); i++) {
-        cout << arr[i] << " ";
+    std::cout << "Final sorted array: ";
+    for (unsigned long long int num : arr) {
+        std::cout << num << " ";
     }
-    cout << endl;
+    std::cout << std::endl;
 
     return 0;
 }
